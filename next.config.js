57ee22e2c829c0d5b34425c1b1af3c6cab1f5103
Louis Parkinson <louisparkinson@gmail.com>
2025-03-12 18:38:28 +0530

    Add explicit webpack and PostCSS configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader'
+      ]
+    });
+    return config;
+  }
 };
 
 module.exports = nextConfig;
