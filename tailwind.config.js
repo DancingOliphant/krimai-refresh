6a532f38b732380131645998f1f154656cbf0711
Louis Parkinson <louisparkinson@gmail.com>
2025-03-12 18:08:08 +0530

    Comprehensive Tailwind CSS and PostCSS configuration reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        'brand-primary': '#YOUR_COLOR_HERE'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
